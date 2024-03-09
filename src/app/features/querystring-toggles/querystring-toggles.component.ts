0424f598c58fa565f34ccfb308729d32d6dc3a76
Miki Ashkenazi <miki.ashkenazi@hibob.io>
2024-03-09 19:27:33 +0200

    remove toggles ability

--- src/app/features/querystring-toggles/querystring-toggles.component.ts
+++ src/app/features/querystring-toggles/querystring-toggles.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+const QSTRING_PARAMS = ['toggles', 'removeToggles'];
+type QParams = { toggles?: string[], removeToggles?: string[] };
+
 @Component({
   selector: 'app-querystring-toggles',
   standalone: true,
@@ -12,13 +15,54 @@ import { Component } from '@angular/core';
 })
 export class QuerystringTogglesComponent {
 
+  paramsObject: QParams = { toggles: [], removeToggles: [] };
+  tabUrl: string;
+
   async ngOnInit() {
     const url = await this.getTabUrl();
-    console.log('tab', url);
+    url && this.extractParams(url);
   }
 
   private async getTabUrl(): Promise<string> {
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    return tab.url || '';
+    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+    this.tabUrl = tab?.url || '';
+    return this.tabUrl;
+  }
+
+  private extractParams(tabUrl: string): QParams | null {
+    const url = new URL(tabUrl);
+
+    if (url.searchParams.size === 0) return null;
+
+    url.searchParams.forEach((value, key) => {
+      if (!QSTRING_PARAMS.includes(key)) return;
+
+      this.paramsObject[key as keyof QParams] = value.split(',');
+    });
+
+    return this.paramsObject;
+  }
+
+  deleteItem(key: keyof QParams, item: string): void {
+    this.paramsObject[key] = this.paramsObject[key].filter((i: string) => i !== item);
+  }
+
+  updateUrl(): void {
+    const updatedUrl = new URL(this.tabUrl);
+    console.log('url', updatedUrl);
+
+    Object.keys(this.paramsObject).forEach((key) => {
+      const valueArray = this.paramsObject[key as keyof QParams];
+
+      if (valueArray?.length > 0) {
+        updatedUrl.searchParams.set(key, valueArray.join(','));
+      } else {
+        updatedUrl.searchParams.delete(key);
+      }
+    });
+
+    console.log('Updated URL', updatedUrl);
+
+    chrome.tabs.update(undefined, { url: updatedUrl.toString() });
   }
 }
--- src/content-script.ts
+++ src/content-script.ts
@@ -1,5 +1,3 @@
-alert('miki content-script ts file');
-
   // const url = chrome.runtime.sendMessage({ action: 'get-current-tab-url' });
   // console.log('Current tab URL:', url);
   // Use the URL for your extension functionality here
